ddf0e961763efa5112f151c78ce73f2ca10d0cd1
steakncheese <romell.bermundo@outlook.com>
2022-12-19 05:55:59 -0700

    integrated camera

--- src/screens/Camera.js
+++ src/screens/Camera.js
@@ -1,41 +1,133 @@
-import { useNavigation } from "@react-navigation/core";
-import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { Camera, CameraType } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+import Button from "../components/Button";
 
-const Camera = () => {
-  const navigation = useNavigation();
+function CameraApp() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      MediaLibrary.requestPermissionsAsync();
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === "granted");
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (cameraRef) {
+      try {
+        const data = await cameraRef.current.takePictureAsync();
+        console.log(data);
+        setImage(data.uri);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
+  const saveImage = async () => {
+    if (image) {
+      try {
+        await MediaLibrary.createAssetAsync(image);
+        alert("Picture save! 🍕");
+        setImage(null);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        onPress={() => navigation.navigate("Home")}
-        style={[styles.button]}
-      >
-        <Text style={styles.buttonText}>Home</Text>
-      </TouchableOpacity>
+      {!image ? (
+        <Camera
+          style={styles.camera}
+          type={type}
+          flashMode={flash}
+          ref={cameraRef}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              padding: 30,
+            }}
+          >
+            <Button
+              icon={"retweet"}
+              onPress={() => {
+                setType(
+                  type === CameraType.back ? CamaraType.front : CameraType.back
+                );
+              }}
+            />
+            <Button
+              icon={"flash"}
+              color={
+                flash === Camera.Constants.FlashMode.off ? "gray" : "#f1f1f1"
+              }
+              onPress={() => {
+                setFlash(
+                  flash === Camera.Constants.FlashMode.off
+                    ? Camera.Constants.FlashMode.on
+                    : Camera.Constants.FlashMode.off
+                );
+              }}
+            />
+          </View>
+        </Camera>
+      ) : (
+        <Image source={{ uri: image }} style={styles.camera} />
+      )}
+      <View>
+        {image ? (
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              paddingHorizontal: 50,
+            }}
+          >
+            <Button
+              title={"Re-take"}
+              icon="retweet"
+              onPress={() => setImage(null)}
+            />
+            <Button title={"Save"} icon="check" onPress={saveImage} />
+          </View>
+        ) : (
+          <Button
+            title={"Take a picture"}
+            icon="camera"
+            onPress={takePicture}
+          />
+        )}
+      </View>
     </View>
   );
-};
+}
 
-export default Camera;
+export default CameraApp;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#000",
     justifyContent: "center",
-    alignItems: "center",
+    padding: 20,
   },
-  button: {
-    backgroundColor: "#0782F9",
-    width: "60%",
-    padding: 15,
-    borderRadius: 10,
-    alignItems: "center",
-    marginTop: 40,
-  },
-  buttonText: {
-    color: "white",
-    fontWeight: "700",
-    fontSize: 16,
+  camera: {
+    flex: 1,
+    borderRadius: 20,
   },
 });
