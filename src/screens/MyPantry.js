42eaba1fff59f8f2bec0c4166a160dd23fa94363
steakncheese <romell.bermundo@outlook.com>
2022-12-21 16:43:56 -0700

    MyPantry has recipes

--- src/screens/MyPantry.js
+++ src/screens/MyPantry.js
@@ -1,41 +1,34 @@
-import { useNavigation } from "@react-navigation/core";
-import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+//import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { useState, useEffect } from "react";
+import { Appbar } from "react-native-paper";
 
 const MyPantry = () => {
-  const navigation = useNavigation();
+  const [meals, setMeals] = useState([]);
+  console.log(meals);
+  const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
+
+  const getMeals = async function () {
+    const response = await fetch(url);
+    const data = await response.json();
+    setMeals(data.categories);
+  };
+
+  useEffect(() => {
+    getMeals();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        onPress={() => navigation.navigate("Home")}
-        style={[styles.button]}
-      >
-        <Text style={styles.buttonText}>Home</Text>
-      </TouchableOpacity>
+      <Appbar>
+        <Appbar.Content title="Recipe Search" />
+      </Appbar>
+      <StatusBar style="auto" />
     </View>
   );
 };
 
 export default MyPantry;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  button: {
-    backgroundColor: "#0782F9",
-    width: "60%",
-    padding: 15,
-    borderRadius: 10,
-    alignItems: "center",
-    marginTop: 40,
-  },
-  buttonText: {
-    color: "white",
-    fontWeight: "700",
-    fontSize: 16,
-  },
-});
+const styles = StyleSheet.create({});
