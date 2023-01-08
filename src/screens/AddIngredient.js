7745e1c5f26c6720c02cad1180516a6cf6eeaa7f
steakncheese <romell.bermundo@outlook.com>
2023-01-07 23:16:41 -0700

    feat: add the ability to add ingredients

--- src/screens/AddIngredient.js
+++ src/screens/AddIngredient.js
@@ -1,41 +1,110 @@
-import { useNavigation } from "@react-navigation/core";
-import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Ingredient from "../components/Ingredient";
 
-const AddIngredient = () => {
-  const navigation = useNavigation();
+export default function App() {
+  const [ingredient, setIngredient] = useState();
+  const [ingredientItems, setIngredientItems] = useState([]);
+
+  const handleAddIngredient = () => {
+    Keyboard.dismiss();
+    setIngredientItems([...ingredientItems, ingredient]);
+    setIngredient(null);
+  };
+
+  const completeIngredient = (index) => {
+    let itemsCopy = [...ingredientItems];
+    itemsCopy.splice(index, 1);
+    setIngredientItems(itemsCopy);
+  };
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        onPress={() => navigation.navigate("Home")}
-        style={[styles.button]}
+      <View style={styles.ingredientsWrapper}>
+        <Text style={styles.sectionTitle}>Your ingredients</Text>
+        <View style={styles.items}>
+          {ingredientItems.map((item, index) => {
+            return (
+              <TouchableOpacity
+                key={index}
+                onPress={() => completeIngredient(index)}
+              >
+                <Ingredient text={item} />
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeIngredientWrapper}
       >
-        <Text style={styles.buttonText}>Home</Text>
-      </TouchableOpacity>
+        <TextInput
+          style={styles.input}
+          placeholder={"Add an ingredient"}
+          value={ingredient}
+          onChangeText={(text) => setIngredient(text)}
+        />
+        <TouchableOpacity onPress={() => handleAddIngredient()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
-};
-
-export default AddIngredient;
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#EBEAED",
+  },
+  ingredientsWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeIngredientWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
     justifyContent: "center",
     alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
   },
-  button: {
-    backgroundColor: "#0782F9",
-    width: "60%",
-    padding: 15,
-    borderRadius: 10,
-    alignItems: "center",
-    marginTop: 40,
-  },
-  buttonText: {
-    color: "white",
-    fontWeight: "700",
-    fontSize: 16,
-  },
+  addText: {},
 });
