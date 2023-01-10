bd846650cc1a591c3185dbb682b826e3f7b1f56a
Jared Huddleston <jaredhud@hotmail.com>
2023-01-10 16:58:03 -0700

    query setup

--- src/components/RecipeSearch.js
+++ src/components/RecipeSearch.js
@@ -1,9 +1,21 @@
 const spoonAPIKey = "332d07c641fd4c11af3cf76f666e3666";
 
-async function complexRecipeSearch(searchCriteria) {
+export async function complexRecipeSearch(
+  searchCriteria,
+  page,
+  resultsPerPage
+) {
   console.log("searchCriteria: ", searchCriteria);
+  let ingredients = searchCriteria.ingredients;
+  let mealType = searchCriteria.mealType;
+  let typeString = ``;
+  if (mealType) {
+    mealType.replace(" ", "%20");
+    typeString = `&type=${mealType}`;
+  }
+  const offset = (page - 1) * resultsPerPage;
   const response = await fetch(
-    `https://api.spoonacular.com/recipes/complexSearch?apiKey=${spoonAPIKey}&query=${searchCriteria}&addRecipeInformation=true`
+    `https://api.spoonacular.com/recipes/complexSearch?apiKey=${spoonAPIKey}&${ingredients}&sort=max-used-ingredients&addRecipeInformation=true&addRecipeNutrition=false&fillIngredients=true${typeString}&offset=${offset}&number=${resultsPerPage}`
   );
   const recipes = await response.json();
   console.log(recipes);
