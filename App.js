857132fbc90b52cab66d7fcc744c3cb70fad6c3f
steakncheese <romell.bermundo@outlook.com>
2023-01-21 20:52:15 -0700

    feat: add bottom navigation

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useFonts } from "expo-font";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import LoginScreen from "./src/screens/LoginScreen";
 import HomeScreen from "./src/screens/HomeScreen";
@@ -25,9 +26,16 @@ import SendEmail from "./src/screens/SendEmail";
 import TestPage from "./src/screens/TestPage";
 
 const Stack = createNativeStackNavigator();
-
+const Tab = createBottomTabNavigator();
+function Home() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Register" component={RegisterPage} />
+    </Tab.Navigator>
+  );
+}
 export default function App() {
-
   const [ingredientList, setIngredientList] = React.useState([]);
 
   const [fontsLoaded] = useFonts({
@@ -45,8 +53,13 @@ export default function App() {
         initialRouteName="LandingPage"
         screenOptions={{ headerShown: false }}
       >
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Register" component={RegisterPage} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+        {/* <Stack.Screen name="Register" component={RegisterPage} /> */}
         <Stack.Screen name="MyPantry" component={MyPantry} />
         <Stack.Screen name="Camera" component={CameraPage} />
         <Stack.Screen name="Voting" component={VotingPage} />
