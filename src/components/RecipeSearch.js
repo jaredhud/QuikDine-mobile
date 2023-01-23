029a86178dac8e540885d8299d7d45a78642b013
Jared Huddleston <jaredhud@hotmail.com>
2023-01-23 10:48:28 -0700

    not sure

--- src/components/RecipeSearch.js
+++ src/components/RecipeSearch.js
@@ -2,26 +2,33 @@ const spoonAPIKey = "332d07c641fd4c11af3cf76f666e3666";
 
 export async function pantryRecipeSearch(searchCriteria, page, resultsPerPage) {
   console.log("searchCriteria: ", searchCriteria);
-  let ingredients = searchCriteria.ingredients;
-  let mealType = searchCriteria.mealType;
-  let cuisine = searchCriteria.cuisine;
+  const query = searchCriteria.query;
+  const ingredients = searchCriteria.ingredients;
+  const mealType = searchCriteria.mealType;
+  const cuisine = searchCriteria.cuisine;
   let ingredientString = ``;
   let typeString = ``;
   let cuisineString = ``;
-  ingredientString = ingredients.join().toLowerCase().replace(/ /g, "%20");
+  if (query.length > 0) {
+    const queryString = `&query=${query}`;
+  } else {
+    const queryString = ``;
+  }
+  if (ingredients.length > 0) {
+    ingredientString = `&includesIngredients=${ingredients
+      .join()
+      .toLowerCase()
+      .replace(/ /g, "%20")}`;
+  }
   console.log(ingredientString);
-  // ingredientString.replace(/ /g, "%20");
-  // console.log(ingredientString);
   if (mealType.length > 0) {
-    mealType.replace(" ", "%20");
-    typeString = `&type=${mealType}`;
+    typeString = `&type=${mealType.replace(/ /g, "%20")}`;
   }
   if (cuisine.length > 0) {
-    cuisine.replace(" ", "%20");
-    cuisineString = `&cuisine=${cuisine}`;
+    cuisineString = `&cuisine=${cuisine.replace(/ /g, "%20")}`;
   }
   const offset = (page - 1) * resultsPerPage;
-  const fetchString = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${spoonAPIKey}&includeIngredients=${ingredientString}&sort=max-used-ingredients&addRecipeInformation=true&addRecipeNutrition=false&fillIngredients=true${typeString}${cuisineString}&offset=${offset}&number=${resultsPerPage}`;
+  const fetchString = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${spoonAPIKey}${ingredientString}&sort=max-used-ingredients&addRecipeInformation=true&addRecipeNutrition=false&fillIngredients=true${typeString}${cuisineString}&offset=${offset}&number=${resultsPerPage}`;
   console.log(fetchString);
   // const response = await fetch(fetchString);
   // const recipes = await response.json();
