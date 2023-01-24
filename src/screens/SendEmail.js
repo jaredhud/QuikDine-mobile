1df87024977bd4a480d40652b3c89e27091c1981
steakncheese <romell.bermundo@outlook.com>
2023-01-24 14:33:19 -0700

    style: create event form fields

--- src/screens/SendEmail.js
+++ src/screens/SendEmail.js
@@ -1,9 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
-import { useEffect, useState } from 'react';
-import * as MailComposer from 'expo-mail-composer';
-import * as Print from 'expo-print';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Button,
+  View,
+} from "react-native";
+import { useEffect, useState } from "react";
+import * as MailComposer from "expo-mail-composer";
+import * as Print from "expo-print";
 
+import { colors } from "../../GlobalStyles";
 
 export default function App() {
   const [isAvailable, setIsAvailable] = useState(false);
@@ -23,14 +31,14 @@ export default function App() {
 
   const sendMail = async () => {
     const { uri } = await Print.printToFileAsync({
-      html: "<h1>Your file</h1>"
+      html: "<h1>Your file</h1>",
     });
 
     MailComposer.composeAsync({
       subject: "QuikDine Event",
       body: body,
       recipients: recipients,
-      attachments: [uri]
+      attachments: [uri],
     });
   };
 
@@ -59,13 +67,46 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <TextInput value={subject} onChangeText={setSubject} placeholder="Subject" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Subject"
+          value={subject}
+          onChangeText={setSubject}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Body"
+          value={body}
+          onChangeText={setBody}
+          style={styles.input}
+          secureTextEntry
+        />
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+      {/* <TextInput
+        value={subject}
+        onChangeText={setSubject}
+        placeholder="Subject"
+      />
       <TextInput value={body} onChangeText={setBody} placeholder="Body" />
-      <TextInput value={email} onChangeText={setEmail} placeholder="Email" />
-      <Button title='Add Recipient' onPress={addRecipient} />
-      <Button title='Remove Recipient' onPress={removeRecipient} />
+      <TextInput value={email} onChangeText={setEmail} placeholder="Email" /> */}
+      <TouchableOpacity onPress={addRecipient} style={styles.button}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+      <Button title="Add Recipient" onPress={addRecipient} />
+      <Button title="Remove Recipient" onPress={removeRecipient} />
       {showRecipients()}
-      {isAvailable ? <Button title='Send Mail' onPress={sendMail} /> : <Text>Email not available</Text>}
+      {isAvailable ? (
+        <Button title="Send Mail" onPress={sendMail} />
+      ) : (
+        <Text>Email not available</Text>
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -74,8 +115,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.lightgreen,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputContainer: {
+    marginTop: "-85%",
+    marginBottom: "0%",
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+    marginBottom: 15,
+  },
+  button: {
+    backgroundColor: "#379540",
+    width: "80%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: "#379540",
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: "#379540",
+    fontWeight: "700",
+    fontSize: 16,
   },
 });
