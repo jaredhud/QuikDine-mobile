ed8829b9cb2baad9293d0a121554f9a3bca9753e
steakncheese <romell.bermundo@outlook.com>
2023-01-29 07:35:34 -0700

    feat: create account page

--- src/screens/MyAccount/AccountHome.js
+++ src/screens/MyAccount/AccountHome.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/core";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity,
+  View,
+  TextInput,
+} from "react-native";
 
 import { Appbar, Searchbar, Card, Paragraph } from "react-native-paper";
 import { auth } from "../../../firebase";
@@ -9,23 +16,145 @@ import { button } from "../../../GlobalStyles";
 
 export const AccountHome = (props) => {
   const navigation = useNavigation();
+  const name = "Sophia";
+  const accountName = "Sophia";
 
   return (
     <View style={styles.container}>
-      <Appbar>
-        <Appbar.Content title="Account Home" />
-      </Appbar>
-      <TouchableOpacity
-        onPress={() => navigation.navigate("Recipe Search")}
-        style={[button]}
+      <View
+        style={{
+          width: "100%",
+          height: "100%",
+          backgroundColor: "white",
+        }}
       >
-        <Text style={styles.buttonText}>To Recipe</Text>
-      </TouchableOpacity>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            padding: 10,
+          }}
+        >
+          {/* <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Ionic name="close-outline" style={{fontSize: 35}} />
+        </TouchableOpacity> */}
+          <Text style={{ fontSize: 16, fontWeight: "bold" }}>Edit Profile</Text>
+          <TouchableOpacity
+            onPress={() => {
+              TostMessage();
+              navigation.goBack();
+            }}
+          >
+            {/* <Ionic
+              name="checkmark"
+              style={{ fontSize: 35, color: "#3493D9" }}
+            /> */}
+          </TouchableOpacity>
+        </View>
+        <View style={{ padding: 20, alignItems: "center" }}>
+          <Image
+            // source={profileImage}
+            style={{ width: 80, height: 80, borderRadius: 100 }}
+          />
+          <Text
+            style={{
+              color: "#3493D9",
+            }}
+          >
+            Change profile photo
+          </Text>
+        </View>
+        <View style={{ padding: 10 }}>
+          <View>
+            <Text
+              style={{
+                opacity: 0.5,
+              }}
+            >
+              Name
+            </Text>
+            <TextInput
+              placeholder="name"
+              defaultValue={name}
+              style={{
+                fontSize: 16,
+                borderBottomWidth: 1,
+                borderColor: "#CDCDCD",
+              }}
+            />
+          </View>
+          <View style={{ paddingVertical: 10 }}>
+            <Text
+              style={{
+                opacity: 0.5,
+              }}
+            >
+              Username
+            </Text>
+            <TextInput
+              placeholder="accountname"
+              defaultValue={accountName}
+              style={{
+                fontSize: 16,
+                borderBottomWidth: 1,
+                borderColor: "#CDCDCD",
+              }}
+            />
+          </View>
+          <View style={{ paddingVertical: 10 }}>
+            <TextInput
+              placeholder="Website"
+              style={{
+                fontSize: 16,
+                borderBottomWidth: 1,
+                borderColor: "#CDCDCD",
+              }}
+            />
+          </View>
+          <View style={{ paddingVertical: 10 }}>
+            <TextInput
+              placeholder="Bio"
+              style={{
+                fontSize: 16,
+                borderBottomWidth: 1,
+                borderColor: "#CDCDCD",
+              }}
+            />
+          </View>
+        </View>
+        <View>
+          <Text
+            style={{
+              marginVertical: 10,
+              padding: 10,
+              color: "#3493D9",
+              borderTopWidth: 1,
+              borderBottomWidth: 1,
+              borderColor: "#EFEFEF",
+            }}
+          >
+            Switch to Professional account
+          </Text>
+          <Text
+            style={{
+              marginVertical: 10,
+              padding: 10,
+              color: "#3493D9",
+              borderTopWidth: 1,
+              borderBottomWidth: 1,
+              borderColor: "#EFEFEF",
+            }}
+          >
+            Persnol information setting
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
 
-export default HomeScreen;
+export default AccountHome;
 
 const styles = StyleSheet.create({
   container: {
