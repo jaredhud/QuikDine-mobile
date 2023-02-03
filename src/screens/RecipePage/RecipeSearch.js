9d652b54adee62a9cc0f029055efbff9b38e51a3
steakncheese <romell.bermundo@outlook.com>
2023-02-02 22:11:48 -0700

    fix: previous and next text height

--- src/screens/RecipePage/RecipeSearch.js
+++ src/screens/RecipePage/RecipeSearch.js
@@ -139,8 +139,9 @@ export const RecipeSearch = (props) => {
           style={{
             flexDirection: "row",
             position: "absolute",
-            bottom: -60,
+            bottom: "-28%",
             justifyContent: "space-between",
+            marginLeft: "3%",
           }}
         >
           <TouchableOpacity
